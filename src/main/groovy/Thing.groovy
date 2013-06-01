3fd44ed10b48ae58c05fa5fb8913b904fcd61dbf
duckweed <andrew.oxenburgh@gmail.com>
2013-06-02 11:21:13 +1200

    some tidying

--- src/main/groovy/Thing.groovy
+++ src/main/groovy/Thing.groovy
@@ -1,2 +1,16 @@
+import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.html.HtmlPage
 
-println "hello, world"
+import static org.junit.Assert.*
+
+WebClient webClient = new WebClient();
+HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
+assertEquals("HtmlUnit - Welcome to HtmlUnit", page.getTitleText());
+
+String pageAsXml = page.asXml();
+assertTrue(pageAsXml.contains("<body class=\"composite\">"));
+
+String pageAsText = page.asText();
+assertTrue(pageAsText.contains("Support for the HTTP and HTTPS protocols"));
+
+webClient.closeAllWindows();
